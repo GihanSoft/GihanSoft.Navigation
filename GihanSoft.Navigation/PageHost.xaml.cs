7960458180514a616119d82d5dc217534f7f875d
Muhammad Babayi <chief.mb1976@gmail.com>
2021-03-02 13:00:40 +0330

    Add project files.

---
+++ GihanSoft.Navigation/PageHost.xaml.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="PageHost.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GihanSoft.Navigation
+{
+    /// <summary>
+    /// Interaction logic for PageHost.xaml.
+    /// </summary>
+    public partial class PageHost
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageHost"/> class.
+        /// </summary>
+        public PageHost()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
